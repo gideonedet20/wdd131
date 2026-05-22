463e4c27693e2f610a377e2d534ca290a725ccd8
Gideon Edet <gideonedet217@gmail.com>
2026-05-22 10:45:44 +0100

    Update styles and add Nigeria flag image
    - Modified CSS variables for primary, secondary, and accent colors in place.css.
    - Added new text color variable for better readability.
    - Enhanced weather section styles with an icon and adjusted SVG display.
    - Introduced a new image file for the Nigeria flag in the images directory.

--- week3/scripts/place.js
+++ week3/scripts/place.js
@@ -1,31 +1,25 @@
-const year = new Date().getFullYear();
-document.getElementById("current-year")
-.textContent = year;
+const temp = 32;
+const speed = 8;
 
-document.getElementById("lastModified")
-.textContent = `Last Modification: ${document.lastModified}`;
-
-const temperature = 32;
-const windSpeed = 8;
-
-function calculateWindChill(temp, speed){
-
+function calculateWindChill(temp, speed) {
     return (
-        13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)
-
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
     ).toFixed(1);
-
 }
 
 let windChill = "N/A";
 
-if (temperature <= 10 && windSpeed > 4.8){
-
-    windChill =`${calculateWindChill(
-        temperature,
-        windSpeed
-    )} °C`;
+if (temp <= 10 && speed > 4.8) {
+    windChill = `${calculateWindChill(temp, speed)} °C`;
 }
 
-document.getElementById("windchill")
-.textContent = windChill;
+document.getElementById("windchill").textContent = windChill;
+
+document.getElementById("current-year").textContent =
+    new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
